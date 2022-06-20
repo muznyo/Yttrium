028b9c26a30867aa0fdcc71ddf8a0ba5f6a64d47
IacobIonut01 <paulionut2003@gmail.com>
2022-06-20 23:05:40 +0300

    Implement History System [1/2]
    Signed-off-by: IacobIonut01 <paulionut2003@gmail.com>

--- Yttrium/DataTransfer.cs
+++ Yttrium/DataTransfer.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Collections.ObjectModel;
 using System.Threading.Tasks;
 using Windows.Data.Xml.Dom;
 using Windows.Storage;
@@ -24,20 +23,80 @@ namespace Yttrium
             XmlElement elsearchterm = doc.CreateElement("searchterm");
             XmlElement elsitename = doc.CreateElement("sitename");
             XmlElement elurl = doc.CreateElement("url");
+            XmlElement eldate = doc.CreateElement("time");
 
             var historyitem = history[0].AppendChild(doc.CreateElement("historyitem"));
 
             historyitem.AppendChild(elsearchterm);
             historyitem.AppendChild(elsitename);
             historyitem.AppendChild(elurl);
+            historyitem.AppendChild(eldate);
 
             elsearchterm.InnerText = searchterm;
             elsitename.InnerText = title;
             elurl.InnerText = url;
+            eldate.InnerText = DateTimeOffset.Now.ToUnixTimeMilliseconds().ToString();
 
             //saves history to settings.xml
             SaveDocument(doc);
 
+        }
+
+        // Parse History
+        public async Task<ObservableCollection<HistoryData>> GetHistoryAsync(string millisStart, string millisEnd)
+        {
+            var arrayList = new ObservableCollection<HistoryData>();
+            //result from documentload method is stored in doc
+            var doc = await DocumentLoad().AsAsyncOperation(); //load xml file
+
+            var history = doc.GetElementsByTagName("historyitem");
+            if (history.Count > 0)
+            {
+                for (int i = 0; i < history.Length; i++)
+                {
+                    var historyitem = history[i];
+                    var historyData = new HistoryData();
+                    for (int j = 0; j < historyitem.ChildNodes.Length; j++) 
+                    {
+                        var child = historyitem.ChildNodes[j];
+                        switch (child.NodeName)
+                        {
+                            case "url":
+                                historyData.URL = child.InnerText.ToString();
+                                break;
+                            case "sitename":
+                                historyData.URLLabel = child.InnerText.ToString();
+                                break;
+                            case "time":
+                                historyData.Timestamp = child.InnerText.ToString();
+                                break;
+                        }
+                    }
+                    if (historyData.isValid)
+                    {
+                        if ((millisStart == null && millisEnd == null) || 
+                        (long.Parse(historyData.Timestamp) <= long.Parse(millisEnd) 
+                        && long.Parse(historyData.Timestamp) >= long.Parse(millisStart)))
+                            arrayList.Add(historyData);
+                    }
+                }
+            }
+            var sortableList = new List<HistoryData>(arrayList);
+            sortableList.Sort((a, b) => { return b.Timestamp.CompareTo(a.Timestamp); });
+
+            for (int i = 0; i < sortableList.Count; i++)
+            {
+                arrayList.Move(arrayList.IndexOf(sortableList[i]), i);
+            }
+            return arrayList;
+        }
+
+        public static DateTime UnixTimeStampToDateTime(double unixTimeStamp)
+        {
+            // Unix timestamp is seconds past epoch
+            DateTime dateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+            dateTime = dateTime.AddSeconds(unixTimeStamp / 1000).ToLocalTime();
+            return dateTime;
         }
 
         private async Task<XmlDocument> DocumentLoad()
--- Yttrium/SettingsPage_History.xaml.cs
+++ Yttrium/SettingsPage_History.xaml.cs
@@ -1,16 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
+using System.Collections.ObjectModel;
 using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
+using System.Threading.Tasks;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
@@ -25,6 +18,86 @@ namespace Yttrium
         public SettingsPage_History()
         {
             this.InitializeComponent();
+
+            
         }
+
+        protected override async void OnNavigatedTo(NavigationEventArgs e)
+        {
+            HistoryData.Source = await GetHistoryGroupedAsync();
+        }   
+
+
+        public static async Task<ObservableCollection<GroupInfoList>> GetHistoryGroupedAsync()
+        {
+            // Grab Contact objects from pre-existing list (list is returned from function GetContactsAsync())
+            var query = from item in await new DataTransfer().GetHistoryAsync(null, null)
+
+                            // Group the items returned from the query, sort and select the ones you want to keep
+                        group item by item.FormattedDate into g
+                        orderby g.Key
+
+                        // GroupInfoList is a simple custom class that has an IEnumerable type attribute, and
+                        // a key attribute. The IGrouping-typed variable g now holds the Contact objects,
+                        // and these objects will be used to create a new GroupInfoList object.
+                        select new GroupInfoList(g) { Key = g.Key };
+
+            return new ObservableCollection<GroupInfoList>(query);
+        }
+
+
+    }
+
+
+    // GroupInfoList class definition:
+    public class GroupInfoList : List<object>
+    {
+        public GroupInfoList(IEnumerable<object> items) : base(items)
+        {
+        }
+        public object Key { get; set; }
+    }
+
+
+    public class HistoryData
+    {
+
+        public Uri ICON { 
+            get
+            {
+                return new Uri("https://www.google.com/s2/favicons?sz=64&domain_url=" + URL);
+            } 
+        }
+        public string URL { get; set; }
+        public string URLLabel { get; set; }
+        public string Timestamp { get; set; }
+
+        public string FormattedDate { get
+            {
+                var todayDate = DateTime.Now.ToString("dddd (dd MMMM yyyy)");
+                var targetDate = DataTransfer.UnixTimeStampToDateTime(double.Parse(Timestamp)).ToString("dddd (dd MMMM yyyy)");
+                switch (targetDate.CompareTo(todayDate))
+                {
+                    case 0: return "Today";
+                    case 1: return "Yesterday";
+                    case 7: return "Last Week";
+                };
+                todayDate = DateTime.Now.ToString("MMMM yyyy");
+                targetDate = DataTransfer.UnixTimeStampToDateTime(double.Parse(Timestamp)).ToString("MMMM");
+                switch (targetDate.CompareTo(todayDate))
+                {
+                    case 0: return "Last Month";
+                    case 1: return "Two Months Ago";
+                };
+                return DataTransfer.UnixTimeStampToDateTime(double.Parse(Timestamp)).ToString("dddd (dd MMMM yyyy)");
+            } 
+        }
+
+        public bool isValid { get
+            {
+                return URL != "" && URLLabel != "" && Timestamp != "";
+            }
+        }
+
     }
 }
